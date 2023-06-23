36e1d606c0a476ab79fbf4df40bc964307aa67b0
Muhammad Saqlain <107932964+saqlain017@users.noreply.github.com>
2023-06-23 15:22:37 +0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
 const navEl = document.getElementById('myNav');
 const nav2_1 = document.getElementById('nav2_1');
 console.log(navEl);
